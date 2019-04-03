fa2d2152525b2ab055eefa1e17ef930ec2c2d412
Gonzalo Urroz <gonzalo.urroz@cursor.cl>
2019-04-03 22:20:19 +1100

    Adds custom merge for segmented

---
+++ cPlus/hashSort/MinHeap.cpp
@@ -0,0 +1,9 @@
+//
+//  MinHeap.cpp
+//  HashSort
+//
+//  Created by Gonzalo Urroz on 3/4/19.
+//  Copyright © 2019 Gonzalo Urroz. All rights reserved.
+//
+
+#include "MinHeap.hpp"
---
+++ cPlus/hashSort/MinHeap.hpp
@@ -0,0 +1,14 @@
+//
+//  MinHeap.hpp
+//  HashSort
+//
+//  Created by Gonzalo Urroz on 3/4/19.
+//  Copyright © 2019 Gonzalo Urroz. All rights reserved.
+//
+
+#ifndef MinHeap_hpp
+#define MinHeap_hpp
+
+#include <stdio.h>
+
+#endif /* MinHeap_hpp */
